2a96bbfd376107853378860f71e006dea247f9de
Saad Majeed <100041901+saad-scytalelabs@users.noreply.github.com>
2022-05-11 20:42:39 +0500

    Delete Contracts/StakingDualRewardsFactory directory

--- Contracts/StakingDualRewardsFactory/pausable_crate/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![no_std]
-
-extern crate alloc;
-
-pub mod data;
-mod pausable;
-
-pub use pausable::PAUSABLE;
--- Contracts/StakingDualRewardsFactory/staking_dual_rewards_crate/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![no_std]
-
-extern crate alloc;
-
-pub mod data;
-mod staking_dual_rewards;
-pub mod entry_points;
-
-pub use staking_dual_rewards::STAKINGDUALREWARDS;

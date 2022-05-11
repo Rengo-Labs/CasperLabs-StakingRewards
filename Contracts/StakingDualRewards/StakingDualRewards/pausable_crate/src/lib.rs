12795ba57fead0799bb87ec81273d85f95ac29a1
Saad Majeed <100041901+saad-scytalelabs@users.noreply.github.com>
2022-05-11 20:44:40 +0500

    QuickSwap Completed

---
+++ Contracts/StakingDualRewards/StakingDualRewards/pausable_crate/src/lib.rs
@@ -0,0 +1,8 @@
+#![no_std]
+
+extern crate alloc;
+
+pub mod data;
+mod pausable;
+
+pub use pausable::PAUSABLE;
---
+++ Contracts/StakingDualRewards/StakingDualRewards/staking_dual_rewards_tests/src/lib.rs
@@ -0,0 +1,5 @@
+#[cfg(test)]
+pub mod staking_dual_rewards_tests;
+
+#[cfg(test)]
+pub mod staking_dual_rewards_instance;

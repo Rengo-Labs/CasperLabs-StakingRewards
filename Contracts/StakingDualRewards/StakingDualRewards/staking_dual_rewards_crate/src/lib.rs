7de3074897a7d768317d05754c5bcea3912c7f3d
Saad Majeed <100041901+saad-scytalelabs@users.noreply.github.com>
2022-05-11 20:46:29 +0500

    Delete Contracts/StakingDualRewards/StakingDualRewards directory

--- Contracts/StakingDualRewards/StakingDualRewards/staking_dual_rewards_crate/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![no_std]
-
-extern crate alloc;
-
-pub mod data;
-mod staking_dual_rewards;
-
-pub use staking_dual_rewards::STAKINGDUALREWARDS;
--- Contracts/StakingDualRewards/StakingDualRewards/staking_dual_rewards_tests/src/lib.rs
+++
@@ -1,5 +0,0 @@
-#[cfg(test)]
-pub mod staking_dual_rewards_tests;
-
-#[cfg(test)]
-pub mod staking_dual_rewards_instance;

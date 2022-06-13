b15f379f75be58ef07a99a2d297df933747508ee
Saad Majeed <100041901+saad-scytalelabs@users.noreply.github.com>
2022-06-13 21:17:27 +0500

    Fixing Event issues

---
+++ Contracts/StakingDualRewards/utils/staking_rewards_utils/src/commons.rs
@@ -0,0 +1,20 @@
+pub mod key_names {
+
+    // keys used globally
+    pub const SELF_ERC20: &str = "self_erc20";
+    pub const RESULT: &str = "result";
+    pub const SELF_PACKAGE_HASH: &str = "package_hash";
+    pub const SELF_CONTRACT_HASH: &str = "self_hash";
+
+    // erc20 key names
+
+    pub const BALANCES_DICT: &str = "balances";
+    pub const NONCES_DICT: &str = "nonces";
+    pub const ALLOWANCES_DICT: &str = "allowances";
+    pub const NAME: &str = "name";
+    pub const SYMBOL: &str = "symbol";
+    pub const DECIMALS: &str = "decimals";
+    pub const TOTAL_SUPPLY: &str = "total_supply";
+    pub const DOMAIN_SEPARATOR: &str = "domain_separator";
+    pub const PERMIT_TYPE_HASH: &str = "permit_type_hash";
+}
